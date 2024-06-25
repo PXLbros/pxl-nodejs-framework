d3656b96f02285380169b5488c763151b6261067
majesticblvd <dennis@pxlagency.com>
2024-06-24 20:39:19 -0700

    Various

--- yalc-publish.mjs
+++ yalc-publish.mjs
@@ -6,23 +6,25 @@ const execAsync = promisify(exec);
 
 async function publish() {
   try {
-    await execAsync('yalc publish');
+    // await execAsync('yalc publish');
 
-    const packageJsonRaw = await readFile('./package.json', { encoding: 'utf8' });
-    const packageJson = JSON.parse(packageJsonRaw);
-    const packageName = packageJson.name;
+    // const packageJsonRaw = await readFile('./package.json', { encoding: 'utf8' });
+    // const packageJson = JSON.parse(packageJsonRaw);
+    // const packageName = packageJson.name;
 
-    const { stdout } = await execAsync(`yalc installations show ${packageName}`);
+    // const { stdout } = await execAsync(`yalc installations show ${packageName}`);
 
-    const matches = stdout.matchAll(/^\s*(\/[\S]+)/gm);
+    // const matches = stdout.matchAll(/^\s*(\/[\S]+)/gm);
 
-    const installationDirectories = Array.from(matches, (m) => m[1]);
+    // const installationDirectories = Array.from(matches, (m) => m[1]);
 
-    for (const installationDirectory of installationDirectories) {
-      console.log(`Run update in ${installationDirectory}`);
+    // for (const installationDirectory of installationDirectories) {
+    //   console.log(`Run update in ${installationDirectory}`);
 
-      await execAsync(`cd ${installationDirectory} && yalc update`);
-    }
+    //   await execAsync(`cd ${installationDirectory} && yalc update`);
+    // }
+
+    await execAsync('yalc push');
 
     console.log('Done!');
   } catch (error) {
