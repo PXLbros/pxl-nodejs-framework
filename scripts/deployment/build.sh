0b970eb94d302ad609eb717a7d65b1c30ad51a8b
majesticblvd <dennis@pxlagency.com>
2024-07-06 08:27:09 -0700

    Don't use source

--- scripts/deployment/build.sh
+++ scripts/deployment/build.sh
@@ -2,14 +2,12 @@
 
 APP_DIR=/home/ec2-user/app/pxl-nodejs-framework
 
-echo "Build... (Directory: $APP_DIR)"
-
 cd $APP_DIR
 
-pwd
-
-# Source ~/.zshrc
-source ~/.zshrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm use
 
