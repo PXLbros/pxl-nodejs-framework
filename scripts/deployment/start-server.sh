017559ee2ac229bb961a75713ae8f4e6800d72ee
majesticblvd <dennis@dreamhuman.com>
2025-06-13 23:21:35 -0700

    Various

--- scripts/deployment/start-server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_DIR=/home/ec2-user/app/docker
-
-cd $DOCKER_DIR
-
-docker-compose up --build --detach --remove-orphans || { echo "Failed to start containers"; exit 1; }
--- scripts/deployment/stop-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-#pm2 stop /home/ec2-user/api/pm2.config.js
